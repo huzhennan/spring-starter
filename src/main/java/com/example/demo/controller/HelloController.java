3e942ffb70da29d2ae2fe468f86b8fb1f69c539b
huzhennan <zhennan.hu@phicomm.com>
2018-02-03 16:51:00 +0800

    add logging

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.mapper.DbMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private DbMapper dbMapper;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello() {
+        logger.info("------------------------------------");
+        logger.info("GET /hello");
+        logger.info("------------------------------------");
         return "hello, world!" + dbMapper.getVersion();
     }
 }
