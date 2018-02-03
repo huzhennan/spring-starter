2f600fd35cc5b20ddfd1047670c220b7ed4d3fd8
huzhennan <zhennan.hu@phicomm.com>
2018-02-03 16:06:23 +0800

    add dao: mybatis

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.mapper.DbMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HelloController {
+    @Autowired
+    private DbMapper dbMapper;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello() {
-        return "hello, world!";
+        return "hello, world!" + dbMapper.getVersion();
     }
 }
---
+++ src/main/java/com/example/demo/mapper/DbMapper.java
@@ -0,0 +1,8 @@
+package com.example.demo.mapper;
+
+/**
+ * Created by zhennan.hu on 18-2-3
+ */
+public interface DbMapper {
+    String getVersion();
+}
